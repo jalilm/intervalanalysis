da37ab8bba26b7ccb4f08c7b358fa4df6c65990d
moraney.jalil@gmail.com <moraney.jalil@gmail.com@1e5ec483-8ef3-549e-5598-a655175f4ec6>
2015-01-06 17:57:41 +0000

    Added support to new tests technqiue.

--- src/intervalAnalysis/Main.java
+++ src/intervalAnalysis/Main.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.Map;
@@ -14,59 +13,33 @@ import soot.BodyTransformer;
 import soot.PackManager;
 import soot.Scene;
 import soot.SootClass;
-import soot.SootMethod;
 import soot.Transform;
 import soot.options.Options;
 import soot.toolkits.graph.BriefUnitGraph;
 
 public class Main {
 
-    static String allTestsFileName = "test_all";
+    static String allTestsFileName = "tests.txt";
     static String testsDescriptorDirecotry = "scripts";
+    static String classpath = ".";
 
     public static void main(String[] args) throws IOException {
-        String testFileName;
-        FileInputStream testFileStream;
+        String classname;
         BufferedReader reader = new BufferedReader(new InputStreamReader(
                 new FileInputStream(testsDescriptorDirecotry + File.separator
                         + allTestsFileName)));
-        while ((testFileName = reader.readLine()) != null) {
-            testFileStream = new FileInputStream(testsDescriptorDirecotry
-                    + File.separator + testFileName);
-            streaming_main(testFileStream, System.out);
-            testFileStream.close();
+        while ((classname = reader.readLine()) != null) {
+            streaming_main(classname, System.out);
         }
         reader.close();
     }
 
-    private static void streaming_main(InputStream input, PrintStream output)
+    private static void streaming_main(String classname, PrintStream output)
             throws IOException {
-        output.println("Jimple Integer Interval Analyzer");
-        output.println("--------------------------------");
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-        output.println("(Current path is: " + System.getProperty("user.dir")
-                + ")");
-        output.print("Enter the classpath of the Class to analyze (use \""
-                + File.pathSeparator + "\" for multiples): ");
-        String classpath = reader.readLine();
-        if (classpath.trim().isEmpty()) {
-            output.println("Empty classpath given, using '.'");
-            classpath = ".";
-        }
-        output.print("Enter the name of the Class to analyze: ");
-        String classname = reader.readLine();
-        output.print("Enter the name of the Method to analyze: ");
-        String methodname = reader.readLine();
-
-        output.println(getRtJarPath() + File.pathSeparator + classpath);
         initializeSoot(getRtJarPath() + File.pathSeparator + classpath,
                 classname);
-        SootClass c = loadClass(classname);
-        @SuppressWarnings("unused")
-        // TODO: how to ask for only one method
-        SootMethod m = c.getMethodByName(methodname);
+        loadClass(classname);
 
         // run soot and print results
         String[] sootArgs = new String[] { classname };
