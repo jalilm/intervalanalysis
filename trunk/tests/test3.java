ec4d4be4c98255958dafb504125e56795a9e8296
moraney.jalil@gmail.com <moraney.jalil@gmail.com@1e5ec483-8ef3-549e-5598-a655175f4ec6>
2015-01-10 15:35:13 +0000

    Removed Warning.

--- trunk/tests/test3.java
+++ trunk/tests/test3.java
@@ -3,7 +3,10 @@ public class test3 {
 
 	public static void main(String args[]) {
 	    int k = 5;
-        int j = 5 - k;
+	    int i = 0;
+	    for (i=0; i < k; i++) {
+	        
+	    }
 	}
 	
 }
